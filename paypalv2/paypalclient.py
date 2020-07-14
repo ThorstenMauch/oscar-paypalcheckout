e841241db1f0da1c7051d87457a35a61c4c1f233
Thorsten Mauch <mauch@imkenberg.com>
2020-07-14 14:49:29 +0200

    fix array_to_json_array to handle objects correctly

--- paypalv2/paypalclient.py
+++ paypalv2/paypalclient.py
@@ -3,6 +3,7 @@ https://developer.paypal.com/docs/api/orders/v2/
 """
 import sys
 
+
 from paypalcheckoutsdk.core import PayPalHttpClient, SandboxEnvironment, LiveEnvironment
 from django.conf import settings
 
@@ -10,7 +11,6 @@ PAYPAL_CLIENT_ID = getattr(settings, 'PAYPAL_CLIENT_ID')
 PAYPAL_CLIENT_SECRET = getattr(settings, 'PAYPAL_CLIENT_SECRET')
 PAYPAL_ENVIRONMENT = getattr(settings, 'PAYPAL_ENVIRONMENT', 'sandbox')
 PAYPAL_DEBUG = getattr(settings, 'PAYPAL_DEBUG', True)
-
 
 class PayPalClient:
     """
@@ -41,20 +41,25 @@ class PayPalClient:
             itr = json_data.__dict__.iteritems()
         else:
             itr = json_data.__dict__.items()
-
+        #pdb.set_trace()
         result = {}
         for key, value in itr:
             # Skip internal attributes.
             if key.startswith("__") or key.startswith("_"):
                 continue
+
             result[key] = self.array_to_json_array(value) if isinstance(value, list) else\
                           self.object_to_json(value) if not self.is_primitive(value) else\
-                          value
+                       value
+
+
         return result
 
     def array_to_json_array(self, json_array) -> list:
+
+
         return [
-            self.object_to_json(item) if self.is_primitive(item)
+            self.object_to_json(item) if not self.is_primitive(item)
             else self.array_to_json_array(item) if isinstance(item, list)
             else item
 
